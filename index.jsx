7c51b0248345e1cd22bc51e1c9becea858aaa207
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-11 12:59:54 -0500

    Reviewed form basics

--- index.jsx
+++ index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/**
+ * Challenge: add another label and input for the password field
+ */
+
 function App() {
   return (
-    <h1>Form goes here</h1>
+    <section>
+      <h1>Signup form</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+
+        <label htmlFor='password'>Password:</label>
+        <input id="password" type='password' name='password' />
+        <br />
+
+        <button>Submit</button>
+
+      </form>
+    </section>
   )
 }
 
