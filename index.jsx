b10629620073ae1b8bbe19355269660b3c0cf218
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-11 13:28:36 -0500

    Form Submission

--- index.jsx
+++ index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/**
- * Challenge: add another label and input for the password field
- */
-
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData(formEl)
+    const email = formData.get("email")
+    console.log(email)
+    // Gather the info from the form
+    // Submit it to a backend
+    formEl.reset()
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form onSubmit={handleSubmit} method="post">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
 
-        <label htmlFor='password'>Password:</label>
-        <input id="password" type='password' name='password' />
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
         <br />
 
         <button>Submit</button>
